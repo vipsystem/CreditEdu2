2ea5665eefd5920e513223bff363bce7af5b43e8
Kevin Austin <kevinaustin@hotmail.com>
2015-12-08 15:53:06 -0800

    organized user model prior to migration

--- db/migrate/20151208212413_create_plans.rb
+++ db/migrate/20151208212413_create_plans.rb
@@ -1,6 +1,10 @@
 class CreatePlans < ActiveRecord::Migration
   def change
     create_table :plans do |t|
+      t.string :name
+      t.string :stripe_id
+      t.string :interval
+      t.integer :amount
 
       t.timestamps null: false
     end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    email "MyString"
-encrypted_password "MyString"
-  end
+    name "Test User"
+    email "test@example.com"
+    password "please123"
 
+    trait :admin do
+      role 'admin'
+    end
+
+  end
 end
